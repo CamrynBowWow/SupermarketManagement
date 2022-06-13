7aff7f92c1be466c09b8b8436ba48e876c57b136
SmallPeenwow <bowdencamryn@gmail.com>
2022-06-14 00:35:14 +0200

    View Categories Complete

--- Supermarket Management System/Startup.cs
+++ Supermarket Management System/Startup.cs
@@ -5,11 +5,14 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Plugins.DataStore.InMemory;
 using Supermarket_Management_System.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UseCases;
+using UseCases.DataStorePluginInterfaces;
 
 namespace Supermarket_Management_System
 {
@@ -29,6 +32,13 @@ namespace Supermarket_Management_System
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            //Dependency Injection for In-Memory Data Store
+            services.AddScoped<ICategoryRepository, CategoryInMemoryRepository>();
+
+            //Dependency Injection for Use Cases and Repositories
+            services.AddTransient<IViewCategoriesUseCase, ViewCategoriesUseCase>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
