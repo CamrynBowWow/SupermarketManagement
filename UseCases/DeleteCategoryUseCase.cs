f103f994af1a25738fbdc2200c0a49264b7e9819
SmallPeenwow <bowdencamryn@gmail.com>
2022-06-15 11:04:53 +0200

    Delete Category Complete

---
+++ UseCases/DeleteCategoryUseCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DataStorePluginInterfaces;
+using UseCases.UseCaseInterfaces;
+
+namespace UseCases
+{
+    public class DeleteCategoryUseCase : IDeleteCategoryUseCase
+    {
+        private readonly ICategoryRepository categoryRepossitory;
+
+        public DeleteCategoryUseCase(ICategoryRepository categoryRepossitory)
+        {
+            this.categoryRepossitory = categoryRepossitory;
+        }
+
+        public void Delete(int categoryId)
+        {
+            categoryRepossitory.DeleteCategory(categoryId);
+        }
+    }
+}
