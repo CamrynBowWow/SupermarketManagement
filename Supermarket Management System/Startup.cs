2ef77a6e34e6c81cd83476f656b7ffcae536c2a9
SmallPeenwow <bowdencamryn@gmail.com>
2022-06-21 23:55:00 +0200

    Edit Product Complete

--- Supermarket Management System/Startup.cs
+++ Supermarket Management System/Startup.cs
@@ -47,6 +47,8 @@ namespace Supermarket_Management_System
             services.AddTransient<IDeleteCategoryUseCase, DeleteCategoryUseCase>();
             services.AddTransient<IViewProductsUseCase, ViewProductsUseCase>();
             services.AddTransient<IAddProductUseCase, AddProductUseCase>();
+            services.AddTransient<IEditProductUseCase, EditProductUseCase>();
+            services.AddTransient<IGetProductByIdUseCase, GetProductByIdUseCase>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
